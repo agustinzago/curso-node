c7c4df04e231c3a58f5301c054765f9679e1162c
Agustin <zagoagus@gmail.com>
2024-03-14 15:54:55 -0300

    Adding winston logger.

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,17 +1,21 @@
-const { getAge, getUUID } = require('./plugins');
-
+// const { getAge, getUUID } = require('./plugins');
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 // const { getUserById } = require('./js-foundation/03-callbacks');
 // const { getUserById } = require('./js-foundation/04-arrow');
 // const { buildMakePerson } = require('./js-foundation/05-factory')
-const getPokemonById = require('./js-foundation/06-promises');
+// const getPokemonById = require('./js-foundation/06-promises')
 
+const { buildLogger } = require('./plugins')
 
-getPokemonById(4)
-  .then( ( pokemon ) => console.log({ pokemon }) )
-  .catch( ( err ) => console.log( err ) )
-  .finally( () => console.log('Finalmente') );
+const logger = buildLogger('app.js')
+
+logger.log('Hola mundo')
+
+// getPokemonById(4)
+//   .then( ( pokemon ) => console.log({ pokemon }) )
+//   .catch( ( err ) => console.log( err ) )
+//   .finally( () => console.log('Finalmente') );
 
 
 
--- 02-bases/src/plugins/index.js
+++ 02-bases/src/plugins/index.js
@@ -1,10 +1,11 @@
 const { getUUID } = require('../plugins/get-id.plugin');
 const { getAge }  = require('../plugins/get-age.plugin');
-
+const buildLogger = require('./logger.plugin')
 const { http } = require('../plugins/http-client.plugin');
 
 module.exports = {
   getAge, 
   getUUID,
   http,
+  buildLogger,
 }
